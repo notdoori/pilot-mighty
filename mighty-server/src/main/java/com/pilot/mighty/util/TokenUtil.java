10bcb9077c3466cdf5b16bd4b5e85df5875bec2e
islee <notdoori@gmail.com>
2021-04-01 11:32:23 +0900

    Token 유효 검사 스킵

--- mighty-server/src/main/java/com/pilot/mighty/util/TokenUtil.java
+++ mighty-server/src/main/java/com/pilot/mighty/util/TokenUtil.java
@@ -9,19 +9,19 @@ public class TokenUtil {
 	
 	public static boolean checkAvailToken(String accessToken) throws ParseException {
 		
-		String[] token = accessToken.split(":");
-		
-		Date oldDateTime = new SimpleDateFormat("yyyyMMddHHmmss").parse(token[1]);
-		Date curDateTime = new Date();
-		
-		long diff = curDateTime.getTime() - oldDateTime.getTime();
-		
-		TimeUnit tu = TimeUnit.SECONDS;
-		long diffTu = tu.convert(diff, TimeUnit.MILLISECONDS);
-			
-		if (diffTu >= 600) {
-			return false;
-		}
+//		String[] token = accessToken.split(":");
+//		
+//		Date oldDateTime = new SimpleDateFormat("yyyyMMddHHmmss").parse(token[1]);
+//		Date curDateTime = new Date();
+//		
+//		long diff = curDateTime.getTime() - oldDateTime.getTime();
+//		
+//		TimeUnit tu = TimeUnit.SECONDS;
+//		long diffTu = tu.convert(diff, TimeUnit.MILLISECONDS);
+//			
+//		if (diffTu >= 600) {
+//			return false;
+//		}
 		
 		return true;
 	}
