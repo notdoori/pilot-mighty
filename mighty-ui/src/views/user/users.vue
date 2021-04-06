dcc7e4b441e34b10c4139fb2a3705d9169a95d5f
jiny1976 <78005421+jiny1976@users.noreply.github.com>
2021-04-06 15:42:32 +0900

    사용자리스트 추가

--- mighty-ui/src/views/user/users.vue
+++ mighty-ui/src/views/user/users.vue
@@ -1,22 +1,33 @@
 <template>
     <div>
-    This is Users Vue !!!
+      <dviv -bind:key="userId" v-for="user in users">
+        <span>{{ user.userId }}</span>
+        <span>{{ user.userName }}</span>
+        <span>{{ user.depart }}</span>
+        <span>{{ user.langType }}</span>
+      </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   data() {
     return {
+      users: []
     }
   },
+  created() {
+    axios.get('/api/users/all')
+      .then(response => this.users = response.data)
+      .catch((error) => console.log(error));
+  },
   methods: {
-  }
 
+  }
 }
 </script>
-
 
 <style>
 
