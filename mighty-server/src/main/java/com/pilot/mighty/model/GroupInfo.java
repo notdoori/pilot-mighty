5fee7093de25f62320cdd4b6efa47913b3927e7c
Your Name <you@example.com>
2021-04-07 17:49:29 +0900

    사용자 그룹 수정 (GroupInfo.java)

--- mighty-server/src/main/java/com/pilot/mighty/model/GroupInfo.java
+++ mighty-server/src/main/java/com/pilot/mighty/model/GroupInfo.java
@@ -2,50 +2,40 @@ package com.pilot.mighty.model;
 
 public class GroupInfo {
 	
-	private String userId;
-	private String userName;
-	private String langType;
-	private String depart;
+	private String groupId;
+	private String groupDesc;
+	private String roleId;
 	
 	public GroupInfo() {}	
 	
-	public GroupInfo(String userId, String userName, String langType, String depart) {
+	public GroupInfo(String groupId, String groupDesc, String roleId) {
 		super();
-		this.userId = userId;
-		this.userName = userName;
-		this.langType = langType;
-		this.depart = depart;
+		this.groupId = groupId;			// 사용자 그룹 아이디
+		this.groupDesc = groupDesc;		// 사용자 그룹 설명
+		this.roleId = roleId;			// 권한 그룹 아이디
 	}
 
-	public String getUserId() {
-		return userId;
+	public String getGroupId() {
+		return groupId;
 	}
 	
-	public void setUserId(String userId) {
-		this.userId = userId;
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
 	}
 	
-	public String getUserName() {
-		return userName;
+	public String getGroupDesc() {
+		return groupDesc;
 	}
 	
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setGroupDesc(String groupDesc) {
+		this.groupDesc = groupDesc;
 	}
 	
-	public String getLangType() {
-		return langType;
+	public String getRoleId() {
+		return roleId;
 	}
 	
-	public void setLangType(String langType) {
-		this.langType = langType;
-	}
-	
-	public String getDepart() {
-		return depart;
-	}
-	
-	public void setDepart(String depart) {
-		this.depart = depart;
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
 	}
 }
