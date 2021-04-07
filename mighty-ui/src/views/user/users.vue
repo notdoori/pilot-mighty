9352958871e14334fdb5f294ec3366bf6eabc8c8
jiny1976 <78005421+jiny1976@users.noreply.github.com>
2021-04-07 09:35:35 +0900

    사용자 리스트 추가

--- mighty-ui/src/views/user/users.vue
+++ mighty-ui/src/views/user/users.vue
@@ -1,34 +1,53 @@
 <template>
     <div>
-      <dviv -bind:key="userId" v-for="user in users">
+      <div v-bind:key="userId" v-for="user in gridData">
         <span>{{ user.userId }}</span>
+        <span>||</span>
         <span>{{ user.userName }}</span>
+        <span>||</span>
         <span>{{ user.depart }}</span>
+        <span>||</span>
         <span>{{ user.langType }}</span>
       </div>
+      <form id="search">
+      Search <input id="queryinput" name="query" v-model="searchQuery">
+      </form>
+      <userGrid :data="gridData" :columns="gridColumns" :filter-key="searchQuery"></userGrid>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import userGrid from '@/views/user/Grid';
 
 export default {
+  name:'User',
+  components: {
+    userGrid
+  },
   data() {
     return {
-      users: []
+      searchQuery: '',
+      gridColumn: ['userId', 'userName', 'depart', 'langType'],
+      gridData: []
     }
   },
   created() {
-    axios.get('/api/users/all')
-      .then(response => this.users = response.data)
-      .catch((error) => console.log(error));
+    this.load()
   },
   methods: {
-
+    load() {
+      axios.get('/api/users/all')
+      .then(res => { this.gridData = res.data;
+      }).catch(e => {
+        console.error(e);
+      })
+    }
   }
 }
 </script>
 
-<style>
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped="">
+  #queryinput {width:310px; margin-left: 10px}
 </style>
