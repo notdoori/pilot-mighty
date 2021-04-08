c6fad7242662a01417c785e1513b23413391eccf
Your Name <you@example.com>
2021-04-08 16:23:04 +0900

    화면 변경 및 팝업 메시지 추가

--- mighty-ui/src/views/user/Authority.vue
+++ mighty-ui/src/views/user/Authority.vue
@@ -29,7 +29,7 @@
               id="queryinput"
               name="query"
               v-model="searchQuery"
-              hint="권한 정보 검색"
+              label="권한 그룹 정보 검색"
               outlined="true"
             ></v-text-field>
             <userGrid
@@ -44,10 +44,15 @@
           <br />
           <div>
             <v-text-field
-              hint="권한 그룹 아이디"
+              label="권한 그룹 아이디"
               outlined="true"
+              v-model="authGroupId"
             ></v-text-field>
-            <v-text-field hint="권한 그룹 설명" outlined="true"></v-text-field>
+            <v-text-field
+              label="권한 그룹 설명"
+              outlined="true"
+              v-model="authGroupDesc"
+            ></v-text-field>
           </div>
           <div>
             <v-btn
@@ -80,7 +85,7 @@
 
     <!-- </v-main> -->
 
-    <v-footer app> 권한 관리 메뉴 화면 입니다. </v-footer>
+    <v-footer app> 권한 그룹 관리 메뉴 화면 입니다. </v-footer>
   </v-app>
 </template>
 
@@ -91,12 +96,12 @@ import userGrid from "@/views/user/Grid";
 export default {
   beforeCreate() {
     /* beforeCreate 훅은 인스턴스가 생성될 때 가장 처음으로 실행되는 훅이다. */
-    alert("beforeCreate() 호출");
+    // alert("beforeCreate() 호출");
   },
   created() {
     /* created 훅은 beforeCreate 훅의 다음 단계로서,
        beforeCreate 훅이 호출된 직후 데이터와 이벤트가 초기화되어 created 훅에서는 데이터와 이벤트에 접근할 수 있다. */
-    alert("created() 호출");
+    // alert("created() 호출");
 
     // 모든 권한 그룹 리스트 조회
     axios
@@ -112,28 +117,28 @@ export default {
   },
   beforeMount() {
     /* beforeMount 훅 이후부터는 컴포넌트에 접근할 수 있다. */
-    alert("beforeMount() 호출");
+    // alert("beforeMount() 호출");
   },
   mounted() {
     /* mounted 훅에서는 인스턴스의 렌더와 DOM 마운트가 끝난 상태이다. */
-    alert("mounted() 호출");
+    // alert("mounted() 호출");
   },
   beforeUpdate() {
     /* 컴포넌트가 마운트가 다 된 후,
        데이터의 감지됐을 때 해당하는 데이터와 관련 있는 DOM 을 업데이트하기 전에 호출된다. */
-    alert("beforeUpdate() 호출");
+    // alert("beforeUpdate() 호출");
   },
   updated() {
     /* updated 훅은 가상 DOM 이 재렌더링 되어 실제 DOM 이 되었을 때 호출된다. */
-    alert("updated() 호출");
+    // alert("updated() 호출");
   },
   beforeDestroy() {
     /* beforeDestroy 훅은 Vue 인스턴스가 제거되기 전에 호출되는 훅이다. */
-    alert("beforeDestroy() 호출");
+    // alert("beforeDestroy() 호출");
   },
   destroyed() {
     /* destroyed 훅은 Vue 인스턴스가 제거된 후에 실행되는 훅이다 */
-    alert("destroyed() 호출");
+    // alert("destroyed() 호출");
   },
   name: "User",
   components: {
@@ -142,8 +147,10 @@ export default {
   data() {
     return {
       searchQuery: "",
-      gridColumns: ["userId", "userName", "depart", "langType"],
-      gridData: [],
+      gridColumns: ["userId", "userName", "depart", "langType"], // 모든 권한 그룹 항목 정보
+      gridData: [], // 모든 권한 그룹 데이터 정보 (SELECT)
+      authGroupId: "", // 권한 그룹 아이디
+      authGroupDesc: "", // 권한 그룹 설명
     };
   },
 
@@ -153,35 +160,65 @@ export default {
 
     // 권한 그룹 추가
     authority_add: function () {
-      axios
-        .post("/api/auth/add", {
-          authId: "KTH_ID_add",
-          authDesc: "KTH_DESC_add",
-        })
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error));
+      if (this.authGroupId === "") {
+        alert("아이디를 입력하여 주십시오.", "");
+      } else {
+        axios
+          .post("/api/auth/add", {
+            authId: this.authGroupId,
+            authDesc: this.authGroupDesc,
+          })
+          .then((response) =>
+            // console.log(response)
+            alert("권한 그룹 추가를 완료하였습니다.")
+          )
+          .catch((error) =>
+            // console.log(error)
+            alert("권한 그룹 추가를 실패하였습니다.")
+          );
+      }
     },
 
     // 권한 그룹 정보 수정
     authority_modify: function () {
-      axios
-        .post("/api/auth/modify", {
-          authId: "KTH_ID_modify",
-          authDesc: "KTH_DESC_modify",
-        })
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error));
+      if (this.authGroupId === "") {
+        alert("아이디를 입력하여 주십시오.");
+      } else {
+        axios
+          .post("/api/auth/modify", {
+            authId: this.authGroupId,
+            authDesc: this.authGroupDesc,
+          })
+          .then((response) =>
+            // console.log(response)
+            alert("권한 그룹 수정을 완료하였습니다.")
+          )
+          .catch((error) =>
+            // console.log(error)
+            alert("권한 그룹 수정을 실패하였습니다.")
+          );
+      }
     },
 
     // 권한 그룹 삭제
     authority_delete: function () {
-      axios
-        .post("/api/auth/delete", {
-          authId: "KTH_ID_delete",
-          authDesc: "KTH_DESC_delete",
-        })
-        .then((response) => console.log(response))
-        .catch((error) => console.log(error));
+      if (this.authGroupId === "") {
+        alert("아이디를 입력하여 주십시오.");
+      } else {
+        axios
+          .post("/api/auth/delete", {
+            authId: this.authGroupId,
+            authDesc: this.authGroupDesc,
+          })
+          .then((response) =>
+            // console.log(response)
+            alert("권한 그룹 삭제를 완료하였습니다.")
+          )
+          .catch((error) =>
+            // console.log(error)
+            alert("권한 그룹 삭제를 실패하였습니다.")
+          );
+      }
     },
   },
 };
--- mighty-ui/src/views/user/Groups.vue
+++ mighty-ui/src/views/user/Groups.vue
@@ -11,7 +11,7 @@
               id="queryinput"
               name="query"
               v-model="searchQuery"
-              hint="사용자 그룹 정보 검색"
+              label="사용자 그룹 정보 검색"
               outlined="true"
             ></v-text-field>
             <userGrid
@@ -26,12 +26,14 @@
           <br />
           <div>
             <v-text-field
-              hint="사용자 그룹 아이디"
+              label="사용자 그룹 아이디"
               outlined="true"
+              v-model="userGroupId"
             ></v-text-field>
             <v-text-field
-              hint="사용자 그룹 설명"
+              label="사용자 그룹 설명"
               outlined="true"
+              v-model="userGroupDesc"
             ></v-text-field>
           </div>
           <div>
@@ -73,16 +75,15 @@ import userGrid from "@/views/user/Grid";
 export default {
   beforeCreate() {
     /* beforeCreate 훅은 인스턴스가 생성될 때 가장 처음으로 실행되는 훅이다. */
-    alert("beforeCreate() 호출");
+    // alert("beforeCreate() 호출");
   },
   created() {
     /* created 훅은 beforeCreate 훅의 다음 단계로서,
        beforeCreate 훅이 호출된 직후 데이터와 이벤트가 초기화되어 created 훅에서는 데이터와 이벤트에 접근할 수 있다. */
-    alert("created() 호출");
+    // alert("created() 호출");
 
     // 모든 사용자 그룹 리스트 조회
     axios
-      // .get("/api/users/all")
       .get("/api/group/all")
       .then(
         (response) => (
@@ -93,28 +94,28 @@ export default {
   },
   beforeMount() {
     /* beforeMount 훅 이후부터는 컴포넌트에 접근할 수 있다. */
-    alert("beforeMount() 호출");
+    // alert("beforeMount() 호출");
   },
   mounted() {
     /* mounted 훅에서는 인스턴스의 렌더와 DOM 마운트가 끝난 상태이다. */
-    alert("mounted() 호출");
+    // alert("mounted() 호출");
   },
   beforeUpdate() {
     /* 컴포넌트가 마운트가 다 된 후,
        데이터의 감지됐을 때 해당하는 데이터와 관련 있는 DOM 을 업데이트하기 전에 호출된다. */
-    alert("beforeUpdate() 호출");
+    // alert("beforeUpdate() 호출");
   },
   updated() {
     /* updated 훅은 가상 DOM 이 재렌더링 되어 실제 DOM 이 되었을 때 호출된다. */
-    alert("updated() 호출");
+    // alert("updated() 호출");
   },
   beforeDestroy() {
     /* beforeDestroy 훅은 Vue 인스턴스가 제거되기 전에 호출되는 훅이다. */
-    alert("beforeDestroy() 호출");
+    // alert("beforeDestroy() 호출");
   },
   destroyed() {
     /* destroyed 훅은 Vue 인스턴스가 제거된 후에 실행되는 훅이다 */
-    alert("destroyed() 호출");
+    // alert("destroyed() 호출");
   },
   name: "User",
   components: {
@@ -123,19 +124,75 @@ export default {
   data() {
     return {
       searchQuery: "",
-      gridColumns: ["userId", "userName", "depart", "langType"],
-      gridData: [],
+      gridColumns: ["userId", "userName", "depart", "langType"], // 모든 사용자 그룹 항목 정보
+      gridData: [], // 모든 사용자 그룹 데이터 정보 (SELECT)
+      userGroupId: "", // 사용자 그룹 아이디
+      userGroupDesc: "", // 사용자 그룹 설명
     };
   },
   methods: {
     // 사용자 그룹 추가
-    user_group_add: function () {},
+    user_group_add: function () {
+      if (this.userGroupId === "") {
+        alert("아이디를 입력하여 주십시오.");
+      } else {
+        axios
+          .post("/api/group/add", {
+            groupId: this.userGroupId,
+            groupDesc: this.userGroupDesc,
+          })
+          .then((response) =>
+            // console.log(response)
+            alert("사용자 그룹 추가를 완료하였습니다.")
+          )
+          .catch((error) =>
+            // console.log(error)
+            alert("사용자 그룹 추가를 실패하였습니다.")
+          );
+      }
+    },
 
     // 사용자 그룹 정보 수정
-    user_group_modify: function () {},
+    user_group_modify: function () {
+      if (this.userGroupId === "") {
+        alert("아이디를 입력하여 주십시오.");
+      } else {
+        axios
+          .post("/api/group/modify", {
+            groupId: this.userGroupId,
+            groupDesc: this.userGroupDesc,
+          })
+          .then((response) =>
+            // console.log(response)
+            alert("사용자 그룹 수정을 완료하였습니다.")
+          )
+          .catch((error) =>
+            // console.log(error)
+            alert("사용자 그룹 수정을 실패하였습니다.")
+          );
+      }
+    },
 
     // 사용자 그룹 삭제
-    user_group_delete: function () {},
+    user_group_delete: function () {
+      if (this.userGroupId === "") {
+        alert("아이디를 입력하여 주십시오.");
+      } else {
+        axios
+          .post("/api/group/delete", {
+            groupId: this.userGroupId,
+            groupDesc: this.userGroupDesc,
+          })
+          .then((response) =>
+            // console.log(response)
+            alert("사용자 그룹 삭제를 완료하였습니다.")
+          )
+          .catch((error) =>
+            // console.log(error)
+            alert("사용자 그룹 삭제를 실패하였습니다.")
+          );
+      }
+    },
   },
 };
 </script>
