045dfaec525f5e544a3b1a91f185f898547cebe8
JoeKim82 <77952075+JoeKim82@users.noreply.github.com>
2021-04-08 09:17:57 +0900

    Update Authority.vue
    style scoped 태그로 변경

--- mighty-ui/src/views/user/Authority.vue
+++ mighty-ui/src/views/user/Authority.vue
@@ -9,7 +9,7 @@
     </v-app-bar> -->
 
     <br />
-    <v-container class="container" row fluid>
+    <v-container class="v-container" row fluid>
       <v-row>
         <v-col>
           <h2>모든 권한 그룹</h2>
@@ -104,7 +104,8 @@ export default {
     // 모든 권한 그룹 리스트 조회
     axios
       // .get("http://127.0.0.1:8080/api/users/all")
-      .get("/api/users/all")
+      //.get("/api/users/all")
+      .get("/api/auth/all")
       .then(
         (response) => (
           (this.gridData = response.data), console.log(this.gridData)
@@ -113,24 +114,56 @@ export default {
       .catch((error) => console.log(error));
   },
   methods: {
+    // 권한 그룹 정보 조회
+    // authority_select: function () {},
+
     // 권한 그룹 추가
-    authority_add: function () {},
+    authority_add: function () {
+      axios
+        .post("/api/auth/add", {
+          authId: "KTH_ID_add",
+          authDesc: "KTH_DESC_add",
+        })
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+    },
 
     // 권한 그룹 정보 수정
-    authority_modify: function () {},
+    authority_modify: function () {
+      axios
+        .post("/api/auth/modify", {
+          authId: "KTH_ID_modify",
+          authDesc: "KTH_DESC_modify",
+        })
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+    },
 
     // 권한 그룹 삭제
-    authority_delete: function () {},
+    authority_delete: function () {
+      axios
+        .post("/api/auth/delete", {
+          authId: "KTH_ID_delete",
+          authDesc: "KTH_DESC_delete",
+        })
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+    },
   },
 };
 </script>
 
+<!--
 <style type="text/css">
-.v-content {
+ .v-content {
   color: red;
 }
+</style>
+-->
+
+<style scoped="">
 .v-container {
-  background-color: grey lighten-3;
+  background-color: grey lighten-1;
 }
 .v-btn {
   margin-right: 10px;
