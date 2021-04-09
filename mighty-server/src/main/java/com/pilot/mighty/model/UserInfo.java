e37dc4cfff746bb3e4bc4cca9c4efca7b07713bd
jiny1976 <78005421+jiny1976@users.noreply.github.com>
2021-04-09 17:05:31 +0900

    사용자 전체 리스트 및 입력폼 연동 추가

--- mighty-server/src/main/java/com/pilot/mighty/model/UserInfo.java
+++ mighty-server/src/main/java/com/pilot/mighty/model/UserInfo.java
@@ -2,21 +2,31 @@ package com.pilot.mighty.model;
 
 public class UserInfo {
 	
+	private int no;
 	private String userId;
 	private String userName;
 	private String langType;
 	private String depart;
+	private String userGroup;
 	
 	public UserInfo() {}	
 	
-	public UserInfo(String userId, String userName, String langType, String depart) {
+	public UserInfo(int no, String userId, String userName, String langType, String depart, String userGroup) {
 		super();
+		this.no = no;
 		this.userId = userId;
 		this.userName = userName;
 		this.langType = langType;
 		this.depart = depart;
+		this.userGroup = userGroup;
 	}
 
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
 	public String getUserId() {
 		return userId;
 	}
@@ -40,5 +50,11 @@ public class UserInfo {
 	}
 	public void setDepart(String depart) {
 		this.depart = depart;
+	}
+	public String getUserGroup() {
+		return userGroup;
+	}
+	public void setUserGroup(String userGroup) {
+		this.userGroup = userGroup;
 	}
 }
