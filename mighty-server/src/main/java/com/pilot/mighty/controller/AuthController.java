5ef5939f62c1d67d4fbf4b16668fe0af7ea035d6
Your Name <you@example.com>
2021-04-09 13:52:29 +0900

    AuthController.java 수정

--- mighty-server/src/main/java/com/pilot/mighty/controller/AuthController.java
+++ mighty-server/src/main/java/com/pilot/mighty/controller/AuthController.java
@@ -21,23 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pilot.mighty.model.AuthInfo;
-import com.pilot.mighty.model.UserInfo;
 import com.pilot.mighty.query.QueryExecutor;
 import com.pilot.mighty.service.AuthService;
-import com.pilot.mighty.service.UserService;
 import com.pilot.mighty.util.TokenUtil;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-//@Api(tags = {"1. User"})
+@Api(tags = {"1. Auth"})
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 	
 	@Autowired
-	UserService userService;
-//	AuthService authService;
+	AuthService authService;
 	
 	@Autowired
 	QueryExecutor qe;
@@ -49,28 +46,18 @@ public class AuthController {
 	 * @author thkim
 	 * @return AuthInfo
 	 */
-	@ApiOperation(value="전체 권한   그룹 조회", notes = "모든 권한 그룹을 조회합니다.")
+	@ApiOperation(value="전체 권한 그룹 조회", notes = "모든 권한 그룹을 조회합니다.")
 	@RequestMapping(value= "/all", method=RequestMethod.GET)
-	public UserInfo[] getUserAll() {
+	public AuthInfo[] getAuthAll() {
 		
-		logger.debug("[AuthController] TEST-001");
+		logger.debug("[AuthController] TEST01");
 		
-		UserInfo[] userInfo = userService.selectUserInfoAll();
+		AuthInfo[] authInfo = authService.selectAuthInfoAll();
 		
-		logger.debug("[AuthController] TEST-002");
+		logger.debug("[AuthController] TEST04");
 		
-		return userInfo;
+		return authInfo;
 	}
-//	public AuthInfo[] getAuthAll() {
-//		
-//		logger.debug("[AuthController] TEST01");
-//		
-//		AuthInfo[] authInfo = authService.selectAuthInfoAll();
-//		
-//		logger.debug("[AuthController] TEST04");
-//		
-//		return authInfo;
-//	}
 	
 	/**
 	 * 권한 그룹 추가 요청 (INSERT)
@@ -92,6 +79,28 @@ public class AuthController {
 		
 		return null;
 	}
+	
+	
+	
+	
+	
+//	@PostMapping(value = "/add"
+//			,consumes = {MediaType.APPLICATION_JSON_VALUE}
+//			,produces = {MediaType.APPLICATION_JSON_VALUE})
+//	@ResponseBody
+//	public ResponseEntity<Object> authAdd(@RequestBody String body) throws JsonParseException, IOException {
+//		
+//		ObjectMapper mapper = new ObjectMapper();
+//		@SuppressWarnings("unchecked")
+//		Map<String, Object> map = mapper.readValue(body, Map.class);
+//		
+//		logger.debug("ROLE_ID: " + map.get("ROLE_ID").toString());
+//		logger.debug("ROLE_DESC: " + map.get("ROLE_DESC").toString());
+//		
+//		AuthInfo[] authInfo = authService.selectAuthInfoAll();
+//		
+//		return null;
+//	}
 
 	/**
 	 * 권한 그룹 수정 요청 (UPDATE)
