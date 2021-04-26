1a745e8e900936966d8e8a15132b76973798e18e
Your Name <you@example.com>
2021-04-26 15:57:21 +0900

    다국어 정보 개선 작업

--- mighty-ui/src/router/MultiLanguage.js
+++ mighty-ui/src/router/MultiLanguage.js
@@ -1,9 +1,23 @@
 import axios from "axios";
 
-const LANGUAGE_PATH = "/api/language";
-const LANGAUGE_CODE = "EN";
+const LANGUAGE_PATH = "/api/language/search";
+const LANGAUGE_CODE = "KO";
 
-let langText = "";
+// ========== (Common) ========== //
+const COMMON_ID = "COMMON_ID"; // 아이디
+const COMMON_DESCRIPTION = "COMMON_DESCRIPTION"; // 설명
+const COMMON_RESET = "COMMON_RESET"; // 초기화
+const COMMON_ADD = "COMMON_ADD"; // 추가
+const COMMON_MODIFY = "COMMON_MODIFY"; // 수정
+const COMMON_DELETE = "COMMON_DELETE"; // 삭제
+// ============================== //
+
+// ========== (Authority.vue) ========== //
+const AUTHORITY_GROUP_LIST = "AUTHORITY_GROUP_LIST"; // 모든 권한 그룹
+const AUTHORITY_GROUP_INFO = "AUTHORITY_GROUP_INFO"; // 권한 그룹 정보
+// ===================================== //
+
+let langText = new Object();
 
 const language = (keyword) => {
     axios
@@ -13,11 +27,42 @@ const language = (keyword) => {
         })
         .then(
             (response) => (
-                (langText = ""),
-                (langText = response.data)
+                (langText["keyword"] = keyword),
+                (langText[langText["keyword"]] = response.data)
             )
         )
         .catch((error) => alert(keyword + "/" + code + "-" + error));
 }
 
-export { langText, language }
+const multiLanguageSet = () => {
+    // 아이디
+    language(COMMON_ID);
+
+    // 설명
+    language(COMMON_DESCRIPTION);
+
+    // 초기화
+    language(COMMON_RESET);
+
+    // 추가
+    language(COMMON_ADD);
+
+    // 수정
+    language(COMMON_MODIFY);
+
+    // 삭제
+    language(COMMON_DELETE);
+
+    // 모든 권한 그룹
+    language(AUTHORITY_GROUP_LIST);
+
+    // 권한 그룹 정보
+    language(AUTHORITY_GROUP_INFO);
+}
+
+const getLangText = (keyword) => {
+    console.log("langText[keyword] : " + langText[keyword])
+    return langText[keyword];
+}
+
+export { multiLanguageSet, getLangText }
