f4eafec3ce8c2a4446ef3ee5ff1709e6bc9a6936
Your Name <you@example.com>
2021-04-27 11:44:07 +0900

    URL 정보 취합 (권한/사용자 그룹 관리)

--- mighty-ui/src/etc/constants-urls.js
+++ mighty-ui/src/etc/constants-urls.js
@@ -6,4 +6,18 @@ export default Object.freeze({
     USER_MODIFY: "/api/users/modify",
     USER_DELETE: "/api/users/delete",
     GROUP_ALL: "/api/group/all",
+
+    // 권한 그룹 관리
+    AUTHORITY_GROUP_ALL: "/api/auth/all",
+    AUTHORITY_GROUP_SEARCH: "/api/auth/search",
+    AUTHORITY_GROUP_ADD: "/api/auth/add",
+    AUTHORITY_GROUP_MODIFY: "/api/auth/modify",
+    AUTHORITY_GROUP_DELETE: "/api/auth/delete",
+
+    // 사용자 그룹 관리
+    USER_GROUP_ALL: "/api/group/all",
+    USER_GROUP_SEARCH: "/api/group/search",
+    USER_GROUP_ADD: "/api/group/add",
+    USER_GROUP_MODIFY: "/api/group/modify",
+    USER_GROUP_DELETE: "/api/group/delete",
 })
